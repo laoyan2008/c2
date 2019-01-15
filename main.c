02a389357e1211c8efb0cd0ed389b226f9ecb885
yy <1019624863@qq.com>
2019-01-15 14:45:38 +0800

    敏感字符系统基本结构框架

--- main.c
+++ main.c
@@ -1,9 +1,51 @@
 #include <stdio.h>
+char arr[1024];
+int index = 0;
+int main()
+{
+    printf("");
 
-int main(){
+    while (1)
+    {
+        printf("1---添加敏感字符\n");
+        printf("2---\n");
+        printf("3---\n");
+        printf("4---\n");
+        printf("5---\n");
 
-    printf("hello world!");
+        printf("");
 
+        int code;
+        scanf("%d", &code);
+
+        if (code == 1)
+        {
+            printf("\n");
+            char m;
+            scanf("%c",&m);
+            scanf("%c",&m);
+
+            arr[index] = m;
+            index++;
+
+            printf("");
+            char x;
+            scanf("%c",&x);
+            scanf("%c",&x);
+        }
+        if (code == 2)
+        {
+        }
+        if (code == 3)
+        {
+        }
+        if (code == 4)
+        {
+        }
+        if (code == 5)
+        {
+            break;
+        }
+    }
     return 0;
-
 }
