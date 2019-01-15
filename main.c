ac7496b4e73314071313c7edb5048d028dae879b
yy <1019624863@qq.com>
2019-01-15 15:00:03 +0800

    敏感字符添加与删除功能

--- main.c
+++ main.c
@@ -3,12 +3,12 @@ char arr[1024];
 int index = 0;
 int main()
 {
-    printf("");
+    printf("进入敏感字符管理系统");
 
     while (1)
     {
         printf("1---添加敏感字符\n");
-        printf("2---\n");
+        printf("2---删除最后一个敏感字符\n");
         printf("3---\n");
         printf("4---\n");
         printf("5---\n");
@@ -20,7 +20,7 @@ int main()
 
         if (code == 1)
         {
-            printf("\n");
+            printf("请输入要添加的敏感字符\n");
             char m;
             scanf("%c",&m);
             scanf("%c",&m);
@@ -28,16 +28,24 @@ int main()
             arr[index] = m;
             index++;
 
-            printf("");
+            printf("添加成功\n");
             char x;
             scanf("%c",&x);
             scanf("%c",&x);
         }
         if (code == 2)
         {
+            printf("删除最后一个敏感字符\n");
+            index--;
+
+            printf("删除成功，回车继续\n");
+            char x;
+            scanf("%c",&x);
+            scanf("%c",&x);
         }
         if (code == 3)
         {
+            
         }
         if (code == 4)
         {
